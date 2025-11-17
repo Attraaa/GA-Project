3a27ef4366ce25d507d9d49d7ebf7bc632c5050a
Jeongseop-Shin <tjql7757@gmail.com>
2025-11-17 22:59:49 +0900

    기초 세팅_pixi구조화

--- Visualization/src/App.jsx
+++ Visualization/src/App.jsx
@@ -1,39 +1,15 @@
 import { useEffect, useRef } from "react";
-import { Application, Assets, Container, Sprite } from "pixi.js";
+import { createPixiApp } from "./pixi/PixiApp";
 
 export default function App() {
+  //여긴 걍 로드만 해주도록 했음
   const pixiRef = useRef(null);
 
   useEffect(() => {
     let app;
+
     (async () => {
-      app = new Application();
-      await app.init({
-        background: "#1099bb",
-        resizeTo: window,
-      });
-
-      pixiRef.current.appendChild(app.canvas);
-
-      const container = new Container();
-      app.stage.addChild(container);
-
-      const texture = await Assets.load("https://pixijs.com/assets/bunny.png");
-
-      for (let i = 0; i < 25; i++) {
-        const bunny = new Sprite(texture);
-        bunny.x = (i % 5) * 40;
-        bunny.y = Math.floor(i / 5) * 40;
-        container.addChild(bunny);
-      }
-
-      container.x = app.screen.width / 2;
-      container.y = app.screen.height / 2;
-      container.pivot.set(container.width / 2, container.height / 2);
-
-      app.ticker.add((time) => {
-        container.rotation -= 0.01 * time.deltaTime;
-      });
+      app = await createPixiApp(pixiRef.current);
     })();
 
     return () => {
---
+++ Visualization/src/pixi/PixiApp.ts
@@ -0,0 +1,19 @@
+import { Application } from "pixi.js";
+import { SceneMain } from "./scenes/SceneMain";
+
+export async function createPixiApp(dom: HTMLElement) {
+  const app = new Application();
+
+  await app.init({
+    //백그라운드 설정
+    background: "#333",
+    resizeTo: window,
+  });
+
+  dom.appendChild(app.canvas);
+
+  const scene = new SceneMain(app);
+  app.stage.addChild(scene.container);
+
+  return app;
+}
