1c94ad4d51e4c6f0eff2a3383e98114db22ab493
Jeongseop-Shin <tjql7757@gmail.com>
2025-11-17 22:53:00 +0900

    수정 전 임시저장

--- Visualization/src/App.jsx
+++ Visualization/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react";
+import { Application, Assets, Container, Sprite } from "pixi.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const pixiRef = useRef(null);
+
+  useEffect(() => {
+    let app;
+    (async () => {
+      app = new Application();
+      await app.init({
+        background: "#1099bb",
+        resizeTo: window,
+      });
+
+      pixiRef.current.appendChild(app.canvas);
+
+      const container = new Container();
+      app.stage.addChild(container);
+
+      const texture = await Assets.load("https://pixijs.com/assets/bunny.png");
+
+      for (let i = 0; i < 25; i++) {
+        const bunny = new Sprite(texture);
+        bunny.x = (i % 5) * 40;
+        bunny.y = Math.floor(i / 5) * 40;
+        container.addChild(bunny);
+      }
+
+      container.x = app.screen.width / 2;
+      container.y = app.screen.height / 2;
+      container.pivot.set(container.width / 2, container.height / 2);
+
+      app.ticker.add((time) => {
+        container.rotation -= 0.01 * time.deltaTime;
+      });
+    })();
+
+    return () => {
+      if (app) app.destroy(true, true);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more예아
-      </p>
-    </>
-  )
+    <div
+      ref={pixiRef}
+      style={{ width: "100vw", height: "100vh", overflow: "hidden" }}
+    />
+  );
 }
-
-export default App
