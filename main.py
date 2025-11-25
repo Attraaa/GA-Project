70c694a8093dadfcdff6ce0a98e4101bd705f982
KKW <kkw@sentry.co.kr>
2025-11-25 13:59:38 +0900

    [fix] 교차 수정, 일부 함수 => 메서드 변경,

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 # 만약 3x3을 또 넣는다면? 가장 낮은곳을 찾아서 들어갈 수 있는 공간이면 쌓아버림
 # [4, 4, 4, 4, 4, 4, 3, 3, 3, 0] 이렇게 되어버림
 # 이제 가장 높은 곳에서 바닥까지의 넓이에서 모든 박스의 넓이를 빼주면 빈 공간이 나옴
-# 만약 컨테이너의 높이를 정한다면 다 쌓았을때 컨테이너의 높이를 넘어가는 유전자에 대해 ㅈㄴ큰 페널티를 주면 됨
+# 만약 컨테이너의 높이를 정한다면 다 쌓았을때 컨테이너의 높이를 넘어가는 유전자에 대해 큰 페널티를 주면 됨
 
 
 # 만약 박스를 다 쌓았는데 [4, 4, 4, 4, 4, 4, 3, 3, 3, 0] 이랑 [4, 3, 3, 3, 3, 3, 3, 3, 3, 0]
@@ -27,22 +27,37 @@ import numpy as np
 # https://tibyte.kr/239/ 이 글이 도움이 될 듯
 
 
-# 교수님도 가중치 디렉토리를 여기에 정의하셨음 우리도 나중에 폴더 만들어서 저장하죠
+
+
+#####################################config#####################################
 data_folder = 'weights'
 
 container_width = 10  # 컨테이너 너비
 
 # 박스 가로 세로 (연구소아님) 개수 딕셔너리로 만들기
 boxes = [
-    {"w": 2, "h": 3, "count": 15},
-    {"w": 2, "h": 4, "count": 15},
-    {"w": 3, "h": 3, "count": 12}
+    {"w": 2, "h": 3, "count": 3},
+    {"w": 2, "h": 4, "count": 2},
+    {"w": 3, "h": 3, "count": 8}
 ]
 
-#모집단 50000개로 해봤는데 지금 boxes 기준으로 최고 점수: 0.3030273278529023 나옴 세대 돌리면 더 올라갈라나?
-population_size = 50000 #모집단 크기
+# 모집단 크기
+population_size = 100000
 
-generation_count = 50 #세대 수
+#부모 후보 수
+# parents = 50
+
+# 세대 수
+generation_count = 50
+
+# 돌연변이 확률
+mutation_rate = 0.02
+
+#####################################config#####################################
+
+
+
+
 
 # 박스 클래스
 class Box:
@@ -112,20 +127,22 @@ class Individual:
         self.fitness = (1.0 / penalty)*10000000
 
         self.wasted_space =  self.area_sum - total_box_area
+#?
 #-----------------------------------------------------------------------
     #copy함수 생성. 독립개체  생성
     def copy(self):
         new_gene = [(box, rot) for (box, rot) in self.gene]
         return Individual(new_gene)
 #-----------------------------------------------------------------------
-    
+
 
 
 
 
 class GeneticAlgorithm:
-    def __init__(self, box_data_list, pop_size=population_size):
+    def __init__(self, box_data_list, pop_size=population_size, mutation_rate=mutation_rate):
         self.pop_size = pop_size
+        self.mutation_rate = mutation_rate
         self.base_boxes = []
         self.population = [] 
         
@@ -150,79 +167,86 @@ class GeneticAlgorithm:
             individual = Individual(gene)
             individual.calculate_fitness()
             self.population.append(individual)#모집단에 추가
-#-----------------------------------------------------------------
-    def eval_population(self):
-        for ind in self.population:
-            ind.calculate_fitness()
-    def select_parent(self,k=5):
-        candidates = np.random.choice(self.population, size=k, replace=False)
-        return max(candidates, key=lambda ind: ind.fitness)
+
+    # def select_parent(self,parents):
+    #     candidates = np.random.choice(self.population, size=parents, replace=False)
+    #     return max(candidates, key=lambda ind: ind.fitness) #후보 중 가장 우수한 개체 하나만
     
+    def swap_mutation(self, individual):
 
-    def swap_mutation(individual, mutation_rate=0.02):
+        if np.random.random() > self.mutation_rate:
+            return
 
-        if np.random.rand() > mutation_rate:
-            return individual
+        #순서 바꾸기
+        idx1, idx2 = np.random.choice(range(len(individual.gene)), 2)
+        individual.gene[idx1], individual.gene[idx2] = individual.gene[idx2], individual.gene[idx1]
 
-        gene = individual.gene
-        length = len(gene)
-        if length < 2:
-            return individual
+        #rotate 바꾸기
+        if np.random.random() < 0.5:
+            box_obj, is_rotated = individual.gene[idx1]
+            individual.gene[idx1] = (box_obj, not is_rotated)
 
-        a, b = np.random.choice(length, 2, replace=False)
-        gene[a], gene[b] = gene[b], gene[a]
+    def order_crossover(self, parent1, parent2):
+        # 유전 알고리즘 교차 정리 참고 http://www.aistudy.com/biology/genetic/operator_moon.htm
+        # 우리는 염색체가 순열로 표시되기 때문에 Order Crossover, PMX중 사용해야험
+        # =========원리정리=========
+        # 두 개의 자름선을 임의로 뽑아 부모1의 유전자를 자르고 그걸 자식한테 그대로 줌.
+        # 그리고 부모1한테 물려받은 부분을 제외한 나머지 부분(중복되면 안되니까)을 부모2를 순회하면서 순서대로 넣음됨
+        # 근데 이게 최선인진 모르겠음.. 바닥부터 쌓으니까 높은 점수의 유전자는 배열의 앞부분이 더 중요할거같은데(추측) 이건 자름선이 완전 랜덤이라 과연 의도한 대로 좋은 자식 유전자를 만들 수 있을까?? 그래서 엘리티즘이 있는건가? ㅁㄹ
+        size = len(parent1.gene)
+        child_gene = [None] * size
 
-        return individual
-
-    def orderCrossover(parent1:Individual,parent2:Individual):
-        offspring1 = parent1.copy()
-        offspring2 = parent2.copy()
-        size = len(offspring1.gene)
-
-        child = [None]*size
+        # 부모1길이 만큼의 인덱스에서 랜덤한 두 수를 뽑아 정렬 후 부모1 슬라이싱해서 자식한테 그 인덱스 그 자리 그대로 물려줌
         start, end = sorted(np.random.choice(size, 2, replace=False))
+        child_gene[start : end + 1] = parent1.gene[start : end + 1]
 
-        child[start:end+1] = offspring1[start:end+1]
+        # 자식한테 이미 들어간 박스들의 ID를 기록해서 중복 방지
+        copied_ids = {item[0].id for item in child_gene[start : end + 1]}
 
-        pos = (end + 1) % size
-        for item in offspring2:
-            if item not in child:
-                child[pos] = item
-                pos = (pos + 1) % size
-
-        offspring = parent1.copy()
-        offspring.gene = child
-        return offspring
-
-    #엘리트 뽑을때 copy사용해야함
-    def elitism_selection(population, num_individuals):
-        individuals = sorted(population, key=lambda ind: ind.fitness, reverse=True)
+        # 부모2 유전자를 처음부터 순회하면서 자식에게 없는 박스만 빈칸에 순서대로 채워 넣음
+        current_idx = 0
+        for gene_item in parent2.gene:
+            box_id = gene_item[0].id
+            
+            if box_id in copied_ids:
+                continue
+            
+            while child_gene[current_idx] is not None:
+                current_idx += 1
+            
+            child_gene[current_idx] = gene_item
+        
+        return Individual(child_gene)
+    
+    #상위 num_individuals개 만큼 뽑기
+    def elitism_selection(self, num_individuals):
+        individuals = sorted(self.population, key=lambda ind: ind.fitness, reverse=True)
         return individuals[:num_individuals]
 
-    def tournament_selection(population, num_individuals, tournament_size=5):
+
+    def tournament_selection(self, num_individuals, tournament_size=5):
         selected = []
-        pop_size = len(population)
 
         for _ in range(num_individuals):
-            candidates = np.random.choice(population, size=min(tournament_size, pop_size), replace=False)
+            candidates = np.random.choice(self.population, size=min(tournament_size, self.pop_size), replace=False)
             best = max(candidates, key=lambda ind: ind.fitness)
             selected.append(best)
 
         return selected
-    def evolve(self, elite_count=5, tournament_size=5, mutation_rate=0.02):
+    
+    def evolve(self, elite_count=5, tournament_size=5):
             new_population = []
 
             elites = self.elitism_selection(self.population, elite_count)
             new_population.extend([e.copy() for e in elites])
 
             while len(new_population) < self.pop_size:
-                p1 = self.tournament_selection(self.population, 1, tournament_size)[0]
-                p2 = self.tournament_selection(self.population, 1, tournament_size)[0]
+                p1 = self.tournament_selection(1, tournament_size)[0]
+                p2 = self.tournament_selection(1, tournament_size)[0]
 
                 child = self.orderCrossover(p1, p2)
 
-                if np.random.rand() < mutation_rate:
-                    self.swap_mutation(child, mutation_rate)
+                self.swap_mutation(child, self.mutation_rate)
 
                 child.calculate_fitness()
                 new_population.append(child)
@@ -240,15 +264,16 @@ for i in range(0, 100):
     print(f"    최대 높이: {ga.population[i].max_height}, 쌓은 면적 합(빈공간 포함, 천장빈공간 미포함): {ga.population[i].area_sum}, 낭비된 공간: {ga.population[i].wasted_space}")
 
 best_individual = max(ga.population, key=lambda ind: ind.fitness)
-print(f"최고 점수: {best_individual.fitness}")
+
+print("\n최고 유전자")
+print(f"적합도 점수 : {best_individual.fitness}")
+print(f"높이: {best_individual.max_height}")
+print(f"쌓은 면적 합(빈공간 포함, 천장빈공간 미포함): {best_individual.area_sum}")
+print(f"낭비된 공간 : {best_individual.wasted_space}")
 
 
 # TODO:
-# 1. 세대 반복
-# 2. 선택
-# 3. 교차
-# 4. 돌연변이
-
+# json 저장 (세대별)
 
 # master_box_list = []
 # current_id = 0
